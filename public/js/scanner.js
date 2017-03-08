ff3ba108694da684805c3b79ccc83f6010dd747d
Dan Murphy <dan.murphy@ft.com>
2017-03-08 14:48:15 +0000

    generated

---
+++ public/js/scanner.js
@@ -0,0 +1,27 @@
+document.getElementById("scan").addEventListener("click", function(evt) {
+	navigator.bluetooth.requestDevice({filters:[{namePrefix: "Puck.js"}]})
+	  .then(function(device) {
+	    console.log(device);
+	    return device.gatt.connect();
+	  })
+	  .then(function(server) {
+	    console.log(server);
+	    return server.getPrimaryService(0x180A);
+	  })
+	  .then(function(service) {
+	    // Step 4: get the Characteristic
+			console.log(`service ${JSON.stringify(service)}`);
+	    return service.getCharacteristic(0x2A29);
+	  })
+	  .then(function(characteristic) {
+			console.log(`characteristic ${JSON.stringify(characteristic)}`);
+	    // Step 5: Write to the characteristic
+	    var data = new Uint8Array([0xbb, 0x25, 0x05, 0x44]);
+	    return characteristic.readValue();
+	  })
+	  .catch(function(error) {
+	     // And of course: error handling!
+			 console.log(`error ${JSON.stringify(error)}`);
+	     console.error('Connection failed!', error);
+	});
+});
